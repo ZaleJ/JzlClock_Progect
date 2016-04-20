0ba11fa7f8e01df7ce6d4f595a04848e53d10850
JZL <947703213@qq.com>
2016-04-20 17:27:42 +0800

    JzlClock

---
+++ app/src/main/java/com/jzlclock/jzl/jzlclock/MainActivity.java
@@ -0,0 +1,23 @@
+package com.jzlclock.jzl.jzlclock;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TabHost;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tabHost = (TabHost) findViewById(R.id.tabHost);
+        tabHost.setup();
+
+        tabHost.addTab(tabHost.newTabSpec("tabTime").setIndicator("时钟").setContent(R.id.tabTime));
+        tabHost.addTab(tabHost.newTabSpec("tabAlarm").setIndicator("闹钟").setContent(R.id.tabAlarm));
+        tabHost.addTab(tabHost.newTabSpec("tabTimer").setIndicator("计时器").setContent(R.id.tabTimer));
+        tabHost.addTab(tabHost.newTabSpec("tabStopWatch").setIndicator("秒表").setContent(R.id.tabStopWatch));
+    }
+
+    private TabHost tabHost;
+}
