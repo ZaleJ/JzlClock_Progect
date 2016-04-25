f73ca2b594bdeebda51af37aeb8f39e21ab258eb
JZL <947703213@qq.com>
2016-04-25 17:01:11 +0800

    JzlClock

--- app/src/main/java/com/jzlclock/jzl/jzlclock/TimerView.java
+++ app/src/main/java/com/jzlclock/jzl/jzlclock/TimerView.java
@@ -1,5 +1,6 @@
 package com.jzlclock.jzl.jzlclock;
 
+import android.app.AlertDialog;
 import android.content.Context;
 import android.text.Editable;
 import android.text.TextUtils;
@@ -12,6 +13,8 @@ import android.widget.LinearLayout;
 
 import java.util.Timer;
 import java.util.TimerTask;
+import android.os.Handler;
+import java.util.logging.LogRecord;
 
 /**
  * Created by Administrator on 2016/4/24 0024.
@@ -30,19 +33,58 @@ public class TimerView extends LinearLayout{
     protected void onFinishInflate() {
         super.onFinishInflate();
 
-
+        btnStart = (Button) findViewById(R.id.btnStart);
         btnPause = (Button) findViewById(R.id.btnPause);
+        btnPause.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+
+                stopTimer();
+
+                btnPause.setVisibility(View.GONE);
+                btnResume.setVisibility(View.VISIBLE);
+            }
+        });
         btnResume = (Button) findViewById(R.id.btnResume);
+        btnResume.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+
+                startTimer();
+
+                btnResume.setVisibility(View.GONE);
+                btnPause.setVisibility(View.VISIBLE);
+            }
+        });
         btnReset = (Button) findViewById(R.id.btnReset);
+        btnReset.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                stopTimer();
+                etHour.setText("0");
+                etMin.setText("0");
+                etSec.setText("0");
+
+                btnStart.setVisibility(View.VISIBLE);     // 可见
+                btnPause.setVisibility(View.GONE);
+                btnReset.setVisibility(View.GONE);
+                btnResume.setVisibility(View.GONE);
+            }
+        });
         etHour = (EditText) findViewById(R.id.etHour);
         etMin = (EditText) findViewById(R.id.etMin);
         etSec = (EditText) findViewById(R.id.etSec);
-        btnStart = (Button) findViewById(R.id.btnStart);
+
 
         btnStart.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
-                // startTimer();
+                 startTimer();
+
+                btnStart.setVisibility(View.GONE);
+                btnPause.setVisibility(View.VISIBLE);
+                //btnResume.setVisibility(View.VISIBLE);
+                btnReset.setVisibility(View.VISIBLE);
             }
         });
 
@@ -130,19 +172,75 @@ public class TimerView extends LinearLayout{
                 ((!TextUtils.isEmpty(etSec.getText()))&&Integer.parseInt(etSec.getText().toString()) > 0));
     }
 
-    /* TODO
+
     private void startTimer(){
         if (timerTask == null){
+            allTimerCount =   Integer.parseInt(etHour.getText().toString()) * 60 * 60 +
+                                Integer.parseInt(etMin.getText().toString()) * 60 +
+                                Integer.parseInt(etSec.getText().toString());       // 计算总时间
             timerTask = new TimerTask() {
                 @Override
                 public void run() {
+                    allTimerCount--;
 
+                    handler.sendEmptyMessage(MSG_WHAT_TIME_IS_TICK);
+
+
+                    if (allTimerCount<=0){
+                        handler.sendEmptyMessage(MSG_WHAT_TIME_IS_UP);
+                        startTimer();
+                    }
                 }
-            }
+            };
+
+            timer.schedule(timerTask, 1000, 1000);     //延迟一秒执行
+
         }
     }
-    */
 
+    private void stopTimer(){
+        if (timerTask != null){
+            timerTask.cancel();
+            timerTask = null;
+        }
+    }
+
+    private Handler handler = new Handler() {
+        public void handleMessage(android.os.Message msg){
+            switch (msg.what){
+                case MSG_WHAT_TIME_IS_TICK:
+
+                    int hour = allTimerCount/60/60;
+                    int min = (allTimerCount/60)%60;
+                    int sec = allTimerCount%60;
+
+                    etHour.setText(hour+"");
+                    etMin.setText(min+"");
+                    etSec.setText(sec+"");
+                    break;
+                case MSG_WHAT_TIME_IS_UP:
+                    if (TIME_IS_UP_BUG_FIXER == 0 || TIME_IS_UP_BUG_FIXER > 1) {
+                        TIME_IS_UP_BUG_FIXER = 1;
+                        new AlertDialog.Builder(getContext()).setTitle("Time is up").setMessage("Time is up").setNegativeButton("Cancel", null).show();
+
+                        btnPause.setVisibility(View.GONE);
+                        btnResume.setVisibility(View.GONE);
+                        btnReset.setVisibility(View.GONE);
+                        btnStart.setVisibility(View.VISIBLE);
+
+                        break;
+                    }else{TIME_IS_UP_BUG_FIXER++;stopTimer();break;}
+                default:
+                    break;
+            }
+
+        };
+    };
+
+    private static final int MSG_WHAT_TIME_IS_UP = 1;
+    private static final int MSG_WHAT_TIME_IS_TICK = 2;
+    private int TIME_IS_UP_BUG_FIXER = 0;
+    private int allTimerCount = 0;
     private Timer timer = new Timer();
     private TimerTask timerTask = null;
     private Button btnStart, btnPause, btnResume, btnReset;
